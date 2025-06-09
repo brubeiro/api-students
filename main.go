57bc44fd6340de8cf0ff29d70fe2233dc1b3db2e
bruno-ribeiro <brubeiro@hotmail.com>
2025-06-09 19:20:28 -0300

    Add initial route

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/students", getStudent)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080")) //alterated
+	//if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+	//slog.Error("failed to start server", "error", err)
+}
+
+// Handler
+func getStudent(c echo.Context) error {
+	return c.String(http.StatusOK, "List of all students!")
+}
